f1be578aff31dbbffc81093c41f231382701abdf
Shravastee Thakur <shravastee@gmail.com>
2025-02-19 21:36:05 +0530

    Integration started

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route, useNavigate } from "react-router";
 import Otp from "./Pages/Otp";
 import Tasks from "./Pages/Tasks";
 import ImportantTasks from "./Pages/ImportantTasks";
 import CompletedTasks from "./Pages/CompletedTasks";
 import IncompleteTasks from "./Pages/IncompleteTasks";
+import { useSelector } from "react-redux";
 
 function App() {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  useEffect(() => {
+    if (isLoggedIn === false) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="h-screen relative">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/otp" element={<Otp />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/important" element={<ImportantTasks />} />
-          <Route path="/completed" element={<CompletedTasks />} />
-          <Route path="/incomplete" element={<IncompleteTasks />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/otp" element={<Otp />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/important" element={<ImportantTasks />} />
+        <Route path="/completed" element={<CompletedTasks />} />
+        <Route path="/incomplete" element={<IncompleteTasks />} />
+      </Routes>
     </div>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,16 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
